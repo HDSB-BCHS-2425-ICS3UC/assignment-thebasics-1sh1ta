99cf5bf2a1c8df453d4a6cd99f39bedf0d8902f1
1sh1ta <1malhotraish@hdsb.ca>
2025-02-25 14:26:51 -0500

    Created examples of the math operations and created print statements for the example equations

---
+++ math_operations.py
@@ -0,0 +1,23 @@
+"""
+Perform Each math operations you have been shown (store the result in a variable) and print the result to the screen.
+Write a comment above each variable describing what the math operation does
+"""
+
+# Math Operations
+sum_result = 10+5 #addtion
+difference_result = 10-5 #subtraction
+product_result = 10*5 #multiplication
+quotient_result = 10/5 #division
+modulus_result = 10%3 #modulus, finding the remainder after division
+exponent_result = 10**2 #exponents, raising one number to the power of another
+import math
+squareroot = math.sqrt(25) #squareroot, 
+
+# Print Statements
+print ("An example of addition is (10+5). The answer is:\n", sum_result)
+print ("An example of subtraction is (10-5). The answer is:\n", difference_result)
+print ("An example of multiplication is (10*5). The answer is:\n", product_result)
+print ("An example of division is (10/5). The answer is:\n", quotient_result)
+print ("An example of finding the modulus is the remainder of (10/3). The modulus is:\n", modulus_result)
+print ("An example of exponents is (10^2). The answer is:\n", exponent_result)
+print ("An example of squarerooting is finding the squareroot of 25. The answer is:\n", squareroot)
