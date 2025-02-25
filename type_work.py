51784ea35c64ecb2bf72edd4271d8815d7b6d7f3
1sh1ta <1malhotraish@hdsb.ca>
2025-02-25 13:37:37 -0500

    Stated definitions for all variables and printed them

---
+++ type_work.py
@@ -0,0 +1,20 @@
+"""
+Have that program declare a type of each type of variable we mentioned in class,
+print it to screen with a message declaring what it is.
+Write a comment above each variable describing the data type
+Difficulty Mark 2
+"""
+
+# Below are the definitions of all the variables
+Integer = ("An integer is any whole number. It can be positive or negative, with a limit of up to about 2 billion.")
+Float = ("A float is a number with decimal points.")
+Character = ("A character is a single character. It can be represented as 'char'.")
+Boolean = ("A boolean is a true or false statement.")
+String = ("A string is a combination of characters.")
+
+# I'm printing the definitions of the variables
+print (Integer)
+print (Float)
+print (Character)
+print (Boolean)
+print (String)
