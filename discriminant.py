a175ee50199a6bc7e42cd4078df5f8fa493432d3
1sh1ta <1malhotraish@hdsb.ca>
2025-03-03 14:14:34 -0500

    Fixed spelling mistakes and casted inputs as floats

--- discriminant.py
+++ discriminant.py
@@ -1,7 +1,7 @@
 
 #Author: Ishita Malhotra
 #Date Modified: March 3, 2025
-#Description: Calculating the discriminant using user input
+#Description: Calculating the discriminant using user input 
 
 
 # Requesting user for the "a" input
