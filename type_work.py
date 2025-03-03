77a4b6e56299890ccb67231f9d6902de62fbc302
1sh1ta <1malhotraish@hdsb.ca>
2025-03-03 14:09:27 -0500

    Assigned types for variables and authored work

--- type_work.py
+++ type_work.py
@@ -5,16 +5,24 @@ Write a comment above each variable describing the data type
 Difficulty Mark 2
 """
 
+Integer = int(5)
+Float = float(5.2)
+Character = str('e')
+Boolean = bool(True)
+String = str("Hello!")
+
+
+
 # Below are the definitions of all the variables
-Integer = ("An integer is any whole number.\nIt can be positive or negative, with a limit of up to about 2 billion.\nAn example of an integer is '5'.\n")
-Float = ("A float is a number with decimal points.\nAn example of a float is '5.2'\n")
-Character = ("A character is a single character. It can be represented as 'char'.\n")
-Boolean = ("A boolean is a true or false statement.\nAn example of a boolean is:\n'Locked In = True'\n")
-String = ("A string is a combination of characters.\nAn example of a string is 'Hello!'")
+integer_definition = (f"An integer is any whole number.\nIt can be positive or negative, with a limit of up to about 2 billion.\nAn example of an integer is {Integer}.\n")
+float_definition = (f"A float is a number with decimal points.\nAn example of a float is {Float}.\n")
+character_definition = (f"A character is a single character. It can be represented as 'char'.\n An example of a character is {Character}.\n")
+boolean_definition = (f"A boolean is a true or false statement.\nAn example of a boolean is: {Boolean}\n")
+string_definition = (f"A string is a combination of characters.\nAn example of a string is {String}")
 
 # I'm printing the definitions of the variables
-print (Integer)
-print (Float)
-print (Character)
-print (Boolean)
-print (String)
+print (integer_definition)
+print (float_definition)
+print (character_definition)
+print (boolean_definition)
+print (string_definition)
